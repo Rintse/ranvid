4144fc8c5798a68ae392ee5d7b49e8d7dcf5750a
rintse <r1ntse.a@gmail.com>
2024-12-13 20:03:12 +0100

    check that doubles are within the right range

--- src/Eval.hs
+++ src/Eval.hs
@@ -3,34 +3,36 @@ module Eval (evalTrip) where
 import Control.Monad.State.Lazy
 import Syntax.Grammar.Abs
 import Control.Applicative (liftA3)
+import Data.InfList (InfList(..))
+import Debug.Trace (trace)
+
+type EvalState = InfList Double
 
 -- evaluate triple of expressions at (x,y), with rng initialized with the seed
-evalTrip :: Trip -> Int -> Int -> State [Double] (Double, Double, Double)
+evalTrip :: Trip -> Int -> Int -> State EvalState (Double, Double, Double)
 evalTrip (Triple a b c) x y = liftA3 (,,) 
     (evalExp a x y) (evalExp b x y) (evalExp c x y)
 
 -- general expressions
-evalExp :: Exp -> Int -> Int -> State [Double] Double
+evalExp :: Exp -> Int -> Int -> State EvalState Double
 evalExp (EVar XVar) x _ = return $ fromIntegral x
 evalExp (EVar YVar) _ y = return $ fromIntegral y
 evalExp (EDVal (Val d)) _ _ = return d
-evalExp Rand _ _ = do
-    draws <- get
-    put $ tail draws
-    return $ head draws
+evalExp Rand _ _ = get >>= (\(r ::: t) -> put t >> return r)
 evalExp (Min e) x y = negate <$> evalExp e x y
 evalExp (Sqrt e) x y = sqrt <$> evalExp e x y
 evalExp (Sin e) x y = sin <$> evalExp e x y
 evalExp (Cos e) x y = cos <$> evalExp e x y
 evalExp (Mul e1 e2) x y = liftA2 (*) (evalExp e1 x y) (evalExp e2 x y)
-evalExp (Div e1 e2) x y = liftA2 (/) (evalExp e1 x y) (evalExp e2 x y)
+evalExp (Div e1 e2) x y = liftA2 divUnit (evalExp e1 x y) (evalExp e2 x y)
+    where divUnit a b = if a < b then a / b else b / a
 evalExp (Add e1 e2) x y = liftA2 (\a b -> a + b / 2) (evalExp e1 x y) (evalExp e2 x y)
 evalExp (Ite c e1 e2) x y = do
     cond <- evalBExp c x y
     if cond then evalExp e1 x y else evalExp e2 x y
 
 -- Boolean expressions
-evalBExp :: BExp -> Int -> Int -> State [Double] Bool
+evalBExp :: BExp -> Int -> Int -> State EvalState Bool
 evalBExp (Eq e1 e2) x y = liftA2 (==) (evalExp e1 x y) (evalExp e2 x y)
 evalBExp (Lt e1 e2) x y =  liftA2 (<) (evalExp e1 x y) (evalExp e2 x y)
 evalBExp (Gt e1 e2) x y =  liftA2 (>) (evalExp e1 x y) (evalExp e2 x y)
--- src/Syntax/Parse.hs
+++ src/Syntax/Parse.hs
@@ -1,19 +1,34 @@
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# LANGUAGE TemplateHaskell, TypeFamilies, LambdaCase #-}
+
 module Syntax.Parse (parse) where
 
+import Data.Functor.Foldable ( cata )
+import Data.Functor.Foldable.TH ( makeBaseFunctor )
 import Syntax.Grammar.Par
 import Syntax.Grammar.Abs
 import System.Exit
 
+makeBaseFunctor ''Exp
+getInvalidDoubles :: Trip -> [Double]
+getInvalidDoubles (Triple a b c) = checkExp a ++ checkExp b ++ checkExp c
+    where checkExp = cata go where
+            go (EDValF (Val d)) = [d | not (d <= 1 && d >= -1)]
+            go other = concat other
+
 -- Parses contents of given input file
 parse :: String -> IO Trip
 parse s = do
-    putStr "Parsing program"
     let ts = myLexer s
     case pTrip ts of
-        Left r -> do
-            putStrLn $ "Parse failed: " ++ r
+        Left err_msg -> do
+            putStrLn $ "Parse failed: " ++ err_msg
             putStrLn $ "Tokens still in stream:\n" ++ show ts
             exitFailure
-        Right r -> do
-            putStr "Parse successful"
-            return r
+        Right triple -> do
+            case getInvalidDoubles triple of
+                [] -> return triple
+                non_empty_list -> do
+                    putStrLn $ "Parsed expression contains numbers outside of"
+                        ++ "the [-1, 1] range: " ++ show non_empty_list
+                    exitFailure
