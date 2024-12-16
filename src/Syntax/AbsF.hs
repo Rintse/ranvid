8d83f3ede38a3a2f094f54aa05ec7ba30215172f
rintse <r1ntse.a@gmail.com>
2024-12-16 22:11:03 +0100

    dont specify strategy for p=1

--- src/Syntax/AbsF.hs
+++ src/Syntax/AbsF.hs
@@ -7,5 +7,7 @@ import Syntax.Grammar.Abs
 
 import Data.Functor.Foldable.TH
 
+-- These base functors allow for the use of `recursion-schemes` on the
+-- generated Exp and BExp trees
 makeBaseFunctor ''Exp
 makeBaseFunctor ''BExp
